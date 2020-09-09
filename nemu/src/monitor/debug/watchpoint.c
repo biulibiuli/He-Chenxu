11d623d916d86dc769b4111f38792438bf3e794f
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-09 07:32:04 +0000

    >  compile NEMU 3018001172 simpleedu Linux simpleedu 4.15.0-115-generic #116-Ubuntu SMP Wed Aug 26 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:32:04 up 1 day, 17:25,  1 user,  load average: 0.65, 0.54, 0.53 24adef6dd205a35cde008fa481bd9ea5b9dc7263

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -1,10 +1,11 @@
 #include "monitor/watchpoint.h"
 #include "monitor/expr.h"
-
+#include "nemu.h"
 #define NR_WP 32
 
 static WP wp_pool[NR_WP];
 static WP *head, *free_;
+
 
 void init_wp_pool() {
 	int i;
@@ -17,7 +18,20 @@ void init_wp_pool() {
 	head = NULL;
 	free_ = wp_pool;
 }
-
+WP* new_wp()
+{
+	WP *f,*p;
+	f = free_;
+	free_ = free_->next;
+	f->next = NULL;
+	p = head;
+	if (p == NULL){head = f;p = head;}
+	else {
+		while (p->next!=NULL)p=p->next;
+		p->next = f;
+		}
+	return f;
+}
 /* TODO: Implement the functionality of watchpoint */
 
 
