db560fb97a875167acad2812a0eb2f307dfbf56f
tracer-ics2016 <tracer@njuics.org>
2020-10-04 04:40:27 +0000

    >  compile NEMU 3018001172 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:40:27 up 7 days, 17:17,  1 user,  load average: 0.75, 1.10, 0.90 ab99d24c7bcf1c97daff89871ca463d33b8c565a

--- nemu/src/cpu/exec/logic/and-template.h
+++ nemu/src/cpu/exec/logic/and-template.h
@@ -4,10 +4,19 @@
 
 static void do_execute () {
 	DATA_TYPE result = op_dest->val & op_src->val;
+	int len = (DATA_BYTE << 3) - 1;
+	int s1,s2;
+	cpu.CF=(result < op_dest->val);
+	cpu.SF=result >> len;
+	s1=op_dest->val>>len;
+	s2=op_src->val>>len;
+    	cpu.OF=(s1 == s2 && s1 != cpu.SF) ;
+	cpu.ZF=!result;
 	OPERAND_W(op_dest, result);
-
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	result ^= result >>4;
+	result ^= result >>2;
+	result ^= result >>1;
+	cpu.PF=!(result & 1);
 
 	print_asm_template2();
 }
