23041e476691ef270280186939e680c93eeb158e
tracer-ics2016 <tracer@njuics.org>
2020-10-27 14:42:37 +0000

    >  compile NEMU 10602121 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:42:37 up 11 days, 13:25,  4 users,  load average: 0.72, 0.49, 0.38 ac4b6ab54c0da8087d3e83e69ccbb4928c42ed10

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,9 +1,25 @@
 #include "common.h"
-
+#include <stdlib.h>
+#include "burst.h"
+#include "cpu/reg.h"
+#define BLOCK_SIZE 64
+#define STORAGE_SIZE_L1 64*1024
+#define STORAGE_SIZE_L2 4*1024*1024
+#define EIGHT_WAY 8
+#define SIXTEEN_WAY 16
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
+extern void ddr3_read(hwaddr_t, void*);
+extern void ddr3_write(hwaddr_t, void*,uint8_t*);
+lnaddr_t seg_translate(swaddr_t, size_t, uint8_t);
+hwaddr_t page_translate(lnaddr_t);
+int is_mmio(hwaddr_t);
+uint32_t mmio_read(hwaddr_t, size_t, int);
+void mmio_write(hwaddr_t, size_t, uint32_t, int);
 /* Memory accessing interfaces */
+CPU_state cpu;
+
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
